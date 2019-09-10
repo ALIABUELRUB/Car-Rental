3e51436452735c15284b621108c3c3b8bd83b7b9
ALIABUELRUB <aliabuelrub0@gmail.com>
2019-09-10 15:19:34 +0300

    Format all files

--- src/axios.js
+++ src/axios.js
@@ -3,10 +3,5 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://car-rental-c13eb.firebaseio.com/',
 })
-// test
-
-// instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
-
-// instance.interceptors.request...
 
 export default instance
